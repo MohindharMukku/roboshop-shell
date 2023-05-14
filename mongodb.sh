91dc461d58b4012f41d1c93ea1459ec9047a5f11
mohi <mohindhar.mukku@outlook.com>
2023-05-14 20:47:05 +0530

    replace 127.0.0.1 with 0.0.0.0

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@ echo -e "\e[35m<<<<<<<<<<<< cping the mongo.repo file  >>>>>>>>>>>>\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[35m<<<<<<<<<<<< chainging the listing address >>>>>>>>>>>>\e[0m"
-sed -i -e 's |127.0.0.1|0.0.0.0|' /etc/yum.repos.d/mongo.repo
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 
 echo -e "\e[35m<<<<<<<<<<<< installing the mongodb >>>>>>>>>>>>\e[0m"
 yum install mongodb-org -y
