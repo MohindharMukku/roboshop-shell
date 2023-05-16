8a3ed9bfabff0bd0a97130679ce7916f00ee0804
mohi <mohindhar.mukku@outlook.com>
2023-05-16 13:39:03 +0530

    sed space error

--- 04.redis.sh
+++ 04.redis.sh
@@ -8,7 +8,7 @@ yum install redis -y
 
 echo -e "\e[35m<<<<<<<<<<<< updating the listening port  >>>>>>>>>>>>\e[0m"
 # update the ip address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
-sed -i -e 's |127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
 echo -e "\e[35m<<<<<<<<<<<< starting the redis service >>>>>>>>>>>>\e[0m"
 systemctl enable redis
