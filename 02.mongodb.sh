724b99d18a770ccdb1cd8e99a04195df5b80819d
mohi <mophindhar.mukku@outlook.com>
2023-05-22 12:34:24 +0530

    status spelling

--- 02.mongodb.sh
+++ 02.mongodb.sh
@@ -4,21 +4,22 @@ source ${common_file_path}/00.common.sh
 
 function_heading "Setting up the mongo.repo file"
 cp 02.mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
-function_staus $?
+function_status $?
+
 
 function_heading "Changing the listing address"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
-function_staus $?
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file  #edit replace 127.0.0.1 with 0.0.0.0
+function_status $?
 
 function_heading "Installing the mongodb"
 yum install mongodb-org -y &>>$log_file
-function_staus $?
+function_status $?
 
 function_heading "starting the mongod"
 systemctl enable mongod &>>$log_file
 systemctl restart mongod &>>$log_file
-function_staus $?
+function_status $?
 
 
-#edit replace 127.0.0.1 with 0.0.0.0
+
 
