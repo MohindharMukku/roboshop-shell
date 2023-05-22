6aa538e1f58647b025b5591d0bac63e3f580c788
mohi <mophindhar.mukku@outlook.com>
2023-05-22 12:21:16 +0530

    final code - mongodb and frontend

--- 02.mongodb.sh
+++ 02.mongodb.sh
@@ -1,16 +1,24 @@
+common_file=$(realpath "$0")
+common_file_path=$(dirname "$common_file")
+source ${"common_file_path"}/00.common.sh
 
-echo -e "\e[35m<<<<<<<<<<<< setting up the mongo.repo file  >>>>>>>>>>>>\e[0m"
-cp /home/centos/roboshop-shell/02.mongo.repo /etc/yum.repos.d/mongo.repo
+function_heading "Setting up the mongo.repo file"
+cp 02.mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+function_staus "$?"
 
-echo -e "\e[35m<<<<<<<<<<<< changing the listing address >>>>>>>>>>>>\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+function_heading "Changing the listing address"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
+function_staus "$?"
 
-echo -e "\e[35m<<<<<<<<<<<< installing the mongodb >>>>>>>>>>>>\e[0m"
-yum install mongodb-org -y
+function_heading "Installing the mongodb"
+yum install mongodb-org -y &>>$log_file
+function_staus "$?"
 
-echo -e "\e[35m<<<<<<<<<<<< starting the mongod >>>>>>>>>>>>\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+function_heading "starting the mongod"
+systemctl enable mongod &>>$log_file
+systemctl restart mongod &>>$log_file
+function_staus "$?"
+
 
 #edit replace 127.0.0.1 with 0.0.0.0
 
