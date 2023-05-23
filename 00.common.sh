8d87941bcdb65f65b87fa32ad26c35edc8bd1daa
mohi <mophindhar.mukku@outlook.com>
2023-05-23 11:40:16 +0530

    status spelling

--- 00.common.sh
+++ 00.common.sh
@@ -27,3 +27,102 @@ fi
 }
 
 #--------------------------------------
+#systemctl and component services  services
+
+function_systemd_setup () {
+  function_heading "coping the service file"
+  cp ${component}.service /etc/systemd/system/${component}.service &>>$log_file
+  function_status $?
+
+  function_heading "Starting the Demon"
+  systemctl daemon-reload &>>$log_file
+  function_status $?
+
+  echo -e "\e[35m<<<<<<<<<<<< starting the catalogue>>>>>>>>>>>>\e[0m"
+  function_heading "starting the $component"
+  systemctl enable $component &>>$log_file
+  systemctl restart $component &>>$log_file
+  function_status $?
+
+}
+
+#-------------------------------------------
+# schema setup for mongodb and my_sql
+function_schema_setup () {
+  if [ "$schema_setup" == "mongo" ]; then
+  function_heading "Configure the mongodb package management system"  #MongoDB Community Edition using the yum package manager
+  cp ${script_dir}/02.mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+   function_status $?
+
+  function_heading "installing the mongodb"
+  yum install -y mongodb-org-shell &>>$log_file
+   function_status $?
+
+  function_heading "load schema"
+  mongo --host mongodb.dev.mohindhar.tech </app/schema/catalogue.js &>>$log_file
+  function_status $?
+  fi
+
+
+  if [ "$schema_setup" == "my_sql" ]; then
+    function_heading " installing the my_sql_client"
+    yum install mysql -y &>>$log_file
+    function_status $?
+
+    function_heading " load schema "
+    mysql -h mysql-dev.rdevopsb72.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>$log_file
+    func_stat_check $?
+  fi
+}
+
+#---------------------------------------
+# app pre-req
+function_app_prereq () {
+  function_heading "create Application user"
+  id ${app_user} &>>$/tmp/roboshop.log
+  if [ $? -ne 0 ]; then
+    useradd ${app_user} &>>/tmp/roboshop.log
+  fi
+  function_status $?
+
+  function_heading "Create Application Directory"
+  rm -rf /app &>>$log_file
+  mkdir /app &>>log_file
+  function_status
+
+  function_heading "Download Application Content"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
+  function_status $?
+
+  function_heading "Extract Application Content"
+  cd /app &>>$log_file
+  unzip unzip /tmp/${component}.zip &>>$log_file
+  function_status $?
+
+  function_heading "Downloading the Dependencies"
+  npm install &>>$log_file
+  function_status $?
+}
+
+
+#----------------------------------------
+#installation of node js
+function_nodejs () {
+  function_heading "configuring NodeJS repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+  function_status $?
+
+  function_heading "install NodeJS"
+  yum install nodejs -y &>>$log_file
+  function_status $?
+
+  function_app_prereq #function in a function
+
+  function_heading "Install NodeJS Dependencies "
+  npm install &>>$log_file
+  function_status $?
+
+  function_schema_setup    #function in a function
+  function_systemd_setup   #function in a function
+
+}
--- 03.catalogue.sh
+++ 03.catalogue.sh
@@ -1,40 +1,11 @@
+common_file=$(realpath "$0") #double quotes is used here to ensure filename as a single entity
+common_file_path=$(dirname "$common_file")
+source ${common_file_path}/00.common.sh
 
-echo -e "\e[35m<<<<<<<<<<<< setting up the node js  >>>>>>>>>>>>\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
 
-echo -e "\e[35m<<<<<<<<<<<< adding the user 'roboshop' >>>>>>>>>>>>\e[0m"
-useradd roboshop
 
-echo -e "\e[35m<<<<<<<<<<<< creating the directory '/app' >>>>>>>>>>>>\e[0m"
-mkdir /app
-
-echo -e "\e[35m<<<<<<<<<<<< Downloading the application code >>>>>>>>>>>>\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-
-echo -e "\e[35m<<<<<<<<<<<< downloading the dependencies  >>>>>>>>>>>>\e[0m"
-cd /app
-npm install
-
-echo -e "\e[35m<<<<<<<<<<<< setting up the SystemD file  >>>>>>>>>>>>\e[0m"
-cp /home/centos/roboshop-shell/03.catalogue.service /etc/systemd/system/catalogue.service
-
-echo -e "\e[35m<<<<<<<<<<<< starting the daemon >>>>>>>>>>>>\e[0m"
-systemctl daemon-reload
-
-echo -e "\e[35m<<<<<<<<<<<< starting the catalogue>>>>>>>>>>>>\e[0m"
-systemctl enable catalogue
-systemctl restart catalogue
-
-echo -e "\e[35m<<<<<<<<<<<< setting up the mongo.repo >>>>>>>>>>>>\e[0m"
-cp /home/centos/roboshop-shell/02.mongo.repo /etc/yum.repos.d/mongo.repo
-
-echo -e "\e[35m<<<<<<<<<<<< installing the mongodd-client >>>>>>>>>>>>\e[0m"
-yum install mongodb-org-shell -y
-
-echo -e "\e[35m<<<<<<<<<<<< loading the schema >>>>>>>>>>>>\e[0m"
-mongo --host mongodb.dev.mohindhar.tech </app/schema/catalogue.js
+component=catalogue
+schema_setup=my_sql
+function_nodejs
 
 #update the catalogue server IP address in frontend file - /etc/nginx/default.d/01.roboshop.conf
