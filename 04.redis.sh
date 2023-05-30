735fba23559d5ffa17e920d1ad7ad62a3e5a806b
mohi <mophindhar.mukku@outlook.com>
2023-05-30 13:15:21 +0530

    note

--- 04.redis.sh
+++ 04.redis.sh
@@ -2,4 +2,5 @@ common_file=$(realpath "$0") #double quotes is used here to ensure filename as a
 common_file_path=$(dirname "$common_file")
 source ${common_file_path}/00.common.sh
 
+service=redis
 function_redis
