45ff5dd300d4d01614ba0fe5635a106c0a75dd9c
mohi <mophindhar.mukku@outlook.com>
2023-05-30 13:56:46 +0530

    note

--- 07.mysql.sh
+++ 07.mysql.sh
@@ -5,7 +5,7 @@ source ${common_file_path}/00.common.sh
 my_sql_root_password=$1
 service=mysql
 
-if [ -z "my_sql_root_password"]; then
+if [ -z "$my_sql_root_password"]; then
        echo "Input mysql root password is missing"
        exit 1
        function_status $?
