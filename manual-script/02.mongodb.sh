c61b65f8c3e7f41bd839763ff423ab87692b4bcc
mohi <mophindhar.mukku@outlook.com>
2023-05-31 16:18:24 +0530

    manual script

--- manual-script/02.mongodb.sh
+++
@@ -1,7 +0,0 @@
-yum install mongodb-org -y
-cp /home/centos/roboshop-shell/manual-script/mongo.repo /etc/yum.repos.d/mongo.repo
-cd /etc/
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
-systemctl enable mongod
-systemctl start mongod
-systemctl restart mongod
