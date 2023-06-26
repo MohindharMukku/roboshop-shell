5ac76593f313f58a70c5a6641ce53f01235568f6
mohi <mohindhar.mukku@outlook.com>
2023-06-26 17:22:15 +0530

    robo shop shell script

--- 04.redis.sh
+++
@@ -1,6 +0,0 @@
-common_file=$(realpath "$0") #double quotes is used here to ensure filename as a single entity
-common_file_path=$(dirname "$common_file")
-source ${common_file_path}/00.common.sh
-
-service=redis
-function_redis
